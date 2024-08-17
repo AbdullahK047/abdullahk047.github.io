d57d3f51b13b1116107e72e6e14622734da2a3a4
Abdullah Khalid <66444446+AbdullahK047@users.noreply.github.com>
2024-08-17 12:47:19 -0500

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -20,7 +20,7 @@ function Home2() {
               MY <span className="purple"> INTRODUCTION </span> 
             </h1>
             <p className="home-about-body">
-              I love automating day-to-day tasks with Robotics and AI!
+              I am tesing how GitHub Pages works. Disregard all information.
               <br />
               <br />Languages I code in:
               <i>
